989123b5750d43141237e929e4a54c7ca185665e
clebersete <carneirosete@gmail.com>
2020-07-16 13:14:58 -0400

    lIBRARY oK

--- test/index.php
+++ test/index.php
@@ -3,7 +3,7 @@
 
 	use notification\Email;
 
-	$novoEmail = new Email(2, "smtp.gmail.com","carneirosete@gmail.com", "97853177", "tls", "587", "sete@gmail.com");
+	$novoEmail = new Email(2, "smtp.gmail.com","carneirosete@gmail.com", "97853177", "tls", "587", "sete@gmail.com", "Equipe de Teste");
 	$novoEmail->sendMail("Assunto de Teste", "<p>Esse eh um e-mail de <b>teste</b>", "carneirosete@gmail.com", "Cleber Sete", "carneirosete@gmail.com", "Sete");
 
 	var_dump($novoEmail);
